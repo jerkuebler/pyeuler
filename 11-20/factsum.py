614bc173d3647b48a77e8535565cf9e1772fd9a9
Jerome Kuebler <jkuebler981@gmail.com>
2017-06-27 12:41:57 -0600

    Solver # 151,613

--- 11-20/factsum.py
+++ 11-20/factsum.py
@@ -4,7 +4,7 @@ import math
 def cheap_solution(fact):
 
     i = math.factorial(fact)
-    digits = str(i).split()
+    digits = str(i)
     return sum(int(x) for x in digits)
 
 print(cheap_solution(100))
