f9e0d21c2e6eccdeca859029f8471cd6f0252bad
Jerome Kuebler <jkuebler981@gmail.com>
2017-07-07 09:48:10 -0600

    Solver # 78,490

--- 21-30/abundant.py
+++ 21-30/abundant.py
@@ -1,3 +1,14 @@
+# Probably not the most efficient use of loops. The memoization makes it run quick anyway. Cheating?
+
+memoize_sums = {}
+
+def memoize(num):
+
+    if num in memoize_sums:
+        return num < memoize_sums[num]
+    else:
+        return sum_proper_divisors(num)
+
 def sum_proper_divisors(num):
     divis = [1]
 
@@ -6,23 +17,24 @@ def sum_proper_divisors(num):
             divis.append(i)
             divis.append(num // i)
     divis = set(divis)
+    memoize_sums[num] = sum(divis)
 
     if sum(divis) > num:
         return True
     else:
         return False
 
-def sum_of_abundance(num):
 
-    for i in range(1, num):
-        if sum_proper_divisors(i):
-            if sum_proper_divisors(num - i):
+def sum_of_abundance(num):
+    for i in range(12, num):
+        if memoize(i):
+            if memoize(num - i):
                 return True
 
     return False
 
-def sum_of_non_abundant_sums(maxnum):
 
+def sum_of_non_abundant_sums(maxnum):
     total = 0
 
     for num in range(maxnum + 1):
@@ -30,5 +42,6 @@ def sum_of_non_abundant_sums(maxnum):
             total += num
 
     return total
+
 
 print(sum_of_non_abundant_sums(28123))
