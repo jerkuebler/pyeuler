6c1a4580b46c18969f81f0662188e74743749280
Jerome Kuebler <jkuebler981@gmail.com>
2017-07-27 08:45:39 -0600

    Solver # 72,414

--- 31-40/digitfact.py
+++ 31-40/digitfact.py
@@ -4,7 +4,6 @@ import math
 
 
 def digit_factorials(num):
-
     total = 0
 
     for i in str(num):
@@ -16,14 +15,15 @@ def digit_factorials(num):
 
     return False
 
-def check_numbers():
 
+def check_numbers():
     digit_fact_list = []
     for i in range(3, 362880):
         if digit_factorials(i):
             digit_fact_list.append(i)
 
     return digit_fact_list
+
 
 digit_fact = check_numbers()
 print(digit_fact)
