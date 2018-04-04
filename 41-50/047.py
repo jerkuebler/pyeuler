b375ac906aebf8c863a6590f8ee684d7b6c949f9
jkuebler <jkuebler981@gmail.com>
2018-04-04 14:51:34 -0600

    Solver #46,878

---
+++ 41-50/047.py
@@ -0,0 +1,51 @@
+def natural_numbers():
+    i = 1
+    while True:
+        i += 1
+        yield i
+
+
+def prime(n):
+    if n < 2:
+        return False
+
+    for i in range(2, int(n ** 0.5 + 1)):
+        if n % i == 0:
+            return False
+
+    return True
+
+
+def prime_factors(n):
+    factors = []
+
+    while not prime(n):
+        for i in range(2, int(n)):
+            if n % i == 0:
+                n /= i
+                factors.append(i)
+                break
+
+    factors.append(int(n))
+
+    return factors
+
+
+if __name__ == '__main__':
+
+    chain_length = 0
+    current_facts = []
+    for i in natural_numbers():
+        facts = prime_factors(i)
+        if len(set(facts)) > 3:
+            chain_length += 1
+            if any(facts) in current_facts:
+                chain_length = 0
+                current_facts = []
+            else:
+                current_facts.extend(facts)
+        else:
+            chain_length = 0
+        if chain_length == 4:
+            print(i - 3)
+            break
